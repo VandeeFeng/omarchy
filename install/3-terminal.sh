abad2b207c6ec9ab8b12eac8524bf04697d0ed17
vandeefeng <naoyanzifzm@hotmail.com>
2025-07-13 02:23:30 +0800

    Vandee

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,5 +2,4 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate bash-completion \
-  ghostty-git
+  man tldr less whois plocate bash-completion
